93cc083543baacb947a981c27e6540195748f0b4
Stefan <139533861+stef20p@users.noreply.github.com>
2025-02-16 08:34:40 +0400

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -5,8 +5,8 @@ from pathlib import Path  # os.path
 
 email = EmailMessage()
 
-email['from'] = 'Stefan Paunovic'
-email['to'] = 'stef20p@gmail.com'
+email['from'] = 'Stef XXXXX'
+email['to'] = 'stxxxxXx@gmail.com'
 email['subject'] = 'you won a million dollars'
 
 email.set_content('USE THIS {link} TO CLAIM YOUR REWARD!')
@@ -14,6 +14,6 @@ email.set_content('USE THIS {link} TO CLAIM YOUR REWARD!')
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('stef20p@gmail.com', 'bqdktwessnyuowmh')
+    smtp.login('stefXXXX@gmail.com', 'password')
     smtp.send_message(email)
     print('all good boss!')
